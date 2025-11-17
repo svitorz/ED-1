31f23d2ff6fab0c85d061af939bfa1a1a49aa8d0
svitorz <vitor.fabio.castro@gmail.com>
2025-11-17 07:33:49 -0300

    feat: setup structs

---
+++ ListaSimplesmenteEncadeada/header.h
@@ -0,0 +1,14 @@
+#ifndef LISTA_SIMPLESMENTE_ENCADEADA_INCLUDED
+#include LISTA_SIMPLESMENTE_ENCADEADA_INCLUDED
+
+struct Node {
+    int value;
+    Node* next;
+};
+
+Struct List {
+  Node* head;
+  int quantity;
+};
+
+#endif // !LISTA_SIMPLESMENTE_ENCADEADA_INCLUDED
